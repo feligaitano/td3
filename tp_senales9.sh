ef2ccecfa56bc93352722dfc197aafb904ba0682
feligaitano <117218001+feligaitano@users.noreply.github.com>
2023-03-30 22:28:09 -0300

    Add files via upload

---
+++ tp_senales9.sh
@@ -0,0 +1,14 @@
+//9. Analice el contenido del archivo sig_02.py y ejecutelo. Que diferencias observa
+respecto al manejo de senales en C?//
+#!/bin/bash
+
+nano sig_02.c //abre el archivo de texto para ver el programa
+make sig_02//compilamos el programa en c
+./sig_02//ejecutamos
+//ctrl+alt+c finaliza el programa
+nano sig_02.py //abre el archivo de texto para ver el programa (se ven pocas diferencias en la sintaxis)
+python3 sig_02.py //ejecuta el programa, hace el mismo proceso que el programa en c
+//ctrl+alt+c finaliza el programa
+
+//ambos programas funcionan igual, con la diferencia de la sintaxis 
+
